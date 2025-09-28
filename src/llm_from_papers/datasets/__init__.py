1648459133cad20f55e601089d326e4f82c6ae49
Thibaut CHAUFFIER <tchauffi@gmail.com>
2025-09-28 18:58:23 +0200

    REFINE: Add type hints and improve docstrings in GPTDatasetV1 class

--- src/llm_from_papers/datasets/__init__.py
+++ src/llm_from_papers/datasets/__init__.py
@@ -1,8 +1,12 @@
 import torch
 from torch.utils.data import Dataset
 
+
 class GPTDatasetV1(Dataset):
-    def __init__(self, text, tokenizer, max_length, stride) -> None:
+    """Custom Dataset for training GPT models.
+    Splits text into overlapping sequences of tokens.
+    """
+    def __init__(self, text: str, tokenizer, max_length: int, stride: int) -> None:
         self.input_ids = []
         self.output_ids = []
 
@@ -16,7 +20,14 @@ class GPTDatasetV1(Dataset):
             self.output_ids.append(target_seq)
 
     def __len__(self):
+        """Return the number of samples in the dataset."""
         return len(self.input_ids)
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int):
+        """Retrieve a sample from the dataset.
+        Args:
+            index: Index of the sample to retrieve.
+        Returns:
+            Tuple of (input_ids, output_ids) as tensors.
+        """
         return torch.Tensor(self.input_ids[index]), torch.Tensor(self.output_ids[index])
